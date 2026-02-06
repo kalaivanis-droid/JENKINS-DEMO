e7511fce04baee635b7a145990eb73b121dfa2ad
kalaivanis-droid <kalaivanis@vit.ac.in>
2026-02-06 12:22:52 +0530

    Add greeting for Azure DevOps

--- Hello.java
+++ Hello.java
@@ -1,5 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello Jenkins Java Lab");
+          System.out.println("Hello AZURE DEVOPS");
     }
 }
